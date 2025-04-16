215ca1a89e3929622db3ca4546d91f926c1e6ea3
tomerbr3456 <59730830+tomerbr3456@users.noreply.github.com>
2025-04-16 13:56:57 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="ai-cutting-edge-application",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+        "pandas>=1.2.0",
+        "numpy>=1.20.0",
+    ],
+    author="",
+    author_email="",
+    description="Experimental ai project using NumPy and Pytest and Django in Python",
+    keywords="ai-cutting-edge-application, python",
+    url="",
+)
